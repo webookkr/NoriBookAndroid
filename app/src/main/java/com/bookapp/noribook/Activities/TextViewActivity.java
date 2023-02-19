522492fed57ea8b54ef7068d45c5a38f7bbe7ee4
우리집 <webookkr@naver.com>
2023-02-20 00:55:41 +0900

    loadpdfdata 분리

--- app/src/main/java/com/bookapp/noribook/Activities/TextViewActivity.java
+++ app/src/main/java/com/bookapp/noribook/Activities/TextViewActivity.java
@@ -1,16 +1,22 @@
 package com.bookapp.noribook.Activities;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bookapp.noribook.MyApplication;
+import com.bookapp.noribook.R;
 import com.bookapp.noribook.databinding.ActivityTextViewBinding;
+import com.bookapp.noribook.databinding.DialogCommentAddBinding;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -33,6 +39,8 @@ public class TextViewActivity extends AppCompatActivity {
     String subTitle, subNumber;
     private ActivityTextViewBinding binding;
 
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,12 +52,38 @@ public class TextViewActivity extends AppCompatActivity {
         subTitle = intent.getStringExtra("subTitle");
         subNumber = intent.getStringExtra("subNumber");
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
         loadBookDetails();
         MyApplication.incrementSubBookViewCount(bookTitle, subNumber);
 
+//        커멘트 보이기 버튼
+        binding.addCommentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                firebase user가 로그인 되어 있어야함
+               if (firebaseAuth.getCurrentUser() == null){
+                   Toast.makeText(TextViewActivity.this, "로그인이 필요합니다.", Toast.LENGTH_SHORT).show();
+               }else{
+                   addCommentDialog();
+                   
+               }
+            }
+        });
+    }
+
+    // 커멘트 달기 다이어로그
+    private String comment ="";
+    private void addCommentDialog() {
+        // inflate bind view for dialog
+        DialogCommentAddBinding commentAddBinding = DialogCommentAddBinding.inflate(LayoutInflater.from(this));
+
+        // setup alert dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.CustomDialog);
 
     }
 
+    // 책정보 가져오기,
     private void loadBookDetails() {
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference("SubBooks/"+bookTitle+"/");
         ref.child(subNumber)
